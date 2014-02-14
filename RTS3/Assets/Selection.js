260385f10706aa8fc2a3d9f1e27b76282e7f1724
Rafael Cosman <rafaelc@stanford.edu>
2014-02-14 13:11:10 -0800

    Initial commit

---
+++ RTS3/Assets/Selection.js
@@ -0,0 +1,44 @@
+﻿#pragma strict
+
+var dragging = false;
+var startPos : Vector2;
+var currentPos : Vector2;
+var gos : GameObject[];
+
+function Start ()
+{
+	
+}
+
+function Update ()
+{
+	
+}
+
+function OnGUI ()
+{
+	if (Input.GetAxisRaw("Fire1") == 1 && Event.current.type == EventType.mouseDrag)
+	{
+       currentPos = Event.current.mousePosition;
+       if (!dragging)
+       {
+         dragging = true;
+         startPos = currentPos;
+       }
+	}
+	else if (Event.current.type == EventType.mouseUp)
+		dragging = false;
+ 
+    if (dragging)
+    {
+       	GUI.Box (new Rect (startPos.x, startPos.y, currentPos.x - startPos.x, currentPos.y - startPos.y), "");
+       	gos = GameObject.FindGameObjectsWithTag("Unit");
+       	for (var go : GameObject in gos)
+       	{
+			if (go.GetComponent(Unit).owner == 1 && go.transform.position.x > startPos.x && go.transform.position.x < currentPos.x - startPos.x && go.transform.position.y > startPos.y && go.transform.position.y < currentPos.y - startPos.y)
+				go.GetComponent(Unit).selected = true;
+			else
+				go.GetComponent(Unit).selected = false;
+		}
+	}
+}
---
+++ RTS3/Assets/Unit.js
@@ -0,0 +1,33 @@
+﻿#pragma strict
+
+var speed = .2;
+var power = 10;
+var vel : Vector2;
+var wayPoints = new ArrayList();
+var selected = false;
+var owner = 1;
+
+function Start ()
+{
+	
+}
+
+function Update ()
+{
+	transform.lossyScale.Set(power / 10, .1, power / 10);
+	if (Input.GetAxisRaw("Fire2") == 1 && selected)
+	{
+		var mousePos : Vector2 = GameObject.Find("Camera").camera.ScreenToWorldPoint(Input.mousePosition);
+		wayPoints.Add(mousePos);
+	}
+	if (wayPoints.Count > 0)
+	{
+		var currentWayPoint : Vector2 = wayPoints[0];
+		vel = currentWayPoint - transform.position;
+		//vel *= 99999999;
+		vel = Vector2.ClampMagnitude(vel, speed);
+		transform.position += vel;
+		if (transform.position == currentWayPoint)
+			wayPoints.RemoveAt(0);
+	}
+}
